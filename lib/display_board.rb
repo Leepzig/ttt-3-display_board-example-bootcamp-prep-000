2aaf5fb4d70db7142a3a54663094a6b8f8178099
Andrew Simmons <neon.duncan@gmail.com>
2021-04-21 14:05:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+board = Array.new(9, "   ")
+
+
+def display_row(arr)
+  puts arr[0] +"|"+ arr[1] + "|" + arr[2]
+
+def display_board
+  puts arr[0] +"|"+ arr[1] + "|" + arr[2]
+  puts arr[3] +"|"+ arr[4] + "|" + arr[5]
+  puts arr[6] +"|"+ arr[7] + "|" + arr[8]
+end
